5f0e6aeaec8c15865baae8b4745a3c9dda0dd41b
Aranzazu Rodriguez <arodriguzr@alumnos.unex.es>
2023-10-21 22:39:27 +0200

    Pantallas principales creadas

--- app/src/main/java/es/unex/nbafantasy/MainActivity.kt
+++ app/src/main/java/es/unex/nbafantasy/MainActivity.kt
@@ -2,10 +2,57 @@ package es.unex.nbafantasy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import es.unex.nbafantasy.databinding.ActivityMainBinding
+import es.unex.nbafantasy.home.EditarFragment
+import es.unex.nbafantasy.home.ListaJugadoresFragment
+import es.unex.nbafantasy.home.PantJuegoFragment
+import es.unex.nbafantasy.home.ResultadoFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpUi()
     }
+
+    fun setUpUi(){
+        binding.bottomNavigation.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+                setOf( R.id.editarFragment,
+                    R.id.pantJuegoFragment,
+                    R.id.listaJugadoresFragment,
+                    R.id.resultadoFragment)
+        )
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    fun setUpListeners(){
+
+
+    }
+
+
 }
