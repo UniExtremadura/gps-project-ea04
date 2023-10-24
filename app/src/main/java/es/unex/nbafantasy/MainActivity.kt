a33edd211b754e313b86458380d800c6f7139156
asanchezhj <asanchezhj@alumnos.unex.es>
2023-10-25 01:27:17 +0200

    modificacion de lo que estaba

--- app/src/main/java/es/unex/nbafantasy/MainActivity.kt
+++ app/src/main/java/es/unex/nbafantasy/MainActivity.kt
@@ -3,6 +3,7 @@ package es.unex.nbafantasy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -18,10 +19,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-
-    private val navController by lazy {
+    private lateinit var navController:NavController
+    /*private val navController by lazy {
         (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
-    }
+
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +33,9 @@ class MainActivity : AppCompatActivity() {
         setUpUi()
     }
 
-    fun setUpUi(){
-        binding.bottomNavigation.setupWithNavController(navController)
+    private fun setUpUi(){
+        initNavigation()
+    /*  binding.bottomNavigation.setupWithNavController(navController)
         appBarConfiguration = AppBarConfiguration(
                 setOf( R.id.editarFragment,
                     R.id.pantJuegoFragment,
@@ -41,7 +44,13 @@ class MainActivity : AppCompatActivity() {
         )
         setSupportActionBar(binding.toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
+*/
+    }
 
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/es/unex/nbafantasy/home/PantJuegoFragment.kt
+++ app/src/main/java/es/unex/nbafantasy/home/PantJuegoFragment.kt
@@ -7,69 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import es.unex.nbafantasy.R
+import es.unex.nbafantasy.databinding.FragmentPantJuegoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PantJuegoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PantJuegoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var botonJuego: Button
-    private lateinit var binding: PantJuegoFragment
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
-
-        //setUpListeners()
-    }
-
-    private fun setUpListeners() {
-        with(binding) {
-
-
-        }
-    }
+    private var _binding:FragmentPantJuegoBinding? = null
+    private val binding get()=_binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pant_juego, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PantJuegoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PantJuegoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        _binding= FragmentPantJuegoBinding.inflate(layoutInflater,container, false)
+        return binding.root
     }
 }
