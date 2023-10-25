4c9369928c7b666e0d2499f334b9a3f64f7e078b
asanchezhj <asanchezhj@alumnos.unex.es>
2023-10-25 10:02:47 +0200

    Boton de navegacion de PantJuego a pantalla juego quitando comentarios

---
+++ app/src/main/java/es/unex/nbafantasy/Juego/PantallaJuegoViewModel.kt
@@ -0,0 +1,9 @@
+package es.unex.nbafantasy.Juego
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PantallaJuegoViewModel @Inject constructor():ViewModel() {
+}
